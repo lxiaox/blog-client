7267342bb2b4baddde57e9712c2420673c78427e
liuxiaoxiao <1512755884@qq.com>
2021-03-12 22:20:35 +0800

    部署脚本

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+rm -rf dist &&
+yarn build &&
+cd dist &&
+git init &&
+git add . &&
+git commot -m "update" &&
+git branch -m master &&
+git remote add origin git@github.com:lxiaox/vue-blog-preview.git &&
+git push -f -u origin master &&
+cd ..
